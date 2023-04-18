502b81a97af7e8aff1ba60811b64d94561877633
unknown <al_bacs@yahoo.com>
2023-04-18 10:14:47 +0800

    adding content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './css/home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div class="container">
+     <div class="card mb-3" id="home-card" style={{maxwidth: 540}}>
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h1 class="card-title"> My name</h1>
+        <h1 class="card-title">is Diane</h1>
+        <p class="card-text">As a psychology graduate, you have developed a strong understanding of the human mind and behavior through rigorous coursework and hands-on experience. You are equipped with skills that allow you to analyze, interpret and communicate complex data and findings related to human behavior.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,15 +7,15 @@ const Nav = () => {
     <div>
 
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Diane Logo</a>
+  <div class="container">
+    <Link className="nav-link"  to ="/">Diane Logo</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
 
       <div className='navbar-nav'>
-        
+
       <Link className="nav-link"  to ="/">Home</Link>
       <Link className="nav-link"  to="/about">About</Link>
       <Link className="nav-link"  to="/contact">Contact</Link>
