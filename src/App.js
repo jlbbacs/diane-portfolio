2e188ca9b058b0ea848cb2e630ed96da119e9f76
unknown <al_bacs@yahoo.com>
2023-04-18 08:16:54 +0800

    builing nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+import Nav from './components/Nav';
+import About from './components/About';
+import Home from './components/Home';
+import Work from './components/Work';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Router>
+    <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/work" element={<Work/>} />
+
+    </Routes>
+    </Router>
   )
 }
 
